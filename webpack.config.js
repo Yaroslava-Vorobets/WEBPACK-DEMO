575022b6707dcb50ec1de579cf3abb340bf2c576
Yaroslava-Vorobets <slavavor85@gmail.com>
2024-04-01 22:15:58 +0200

    index.html

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,11 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
+
 module.exports = {
-  context: path.resolve(__dirname,'src',),
+  // context: path.resolve(__dirname,'src',),
   mode: 'development',
-  entry: './index.js',
+  entry: './src/index.js',
   output: {
     filename: 'main.js',
       path: path.resolve(__dirname, 'dist'),
